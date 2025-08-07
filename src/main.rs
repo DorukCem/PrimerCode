97e835cf0d36e7002a574cf28fa4792b6bdbb8e9
Doruk <doruk.cem@gmail.com>
2025-08-07 19:26:22 +0300

    env var from .env

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ mod types;
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = dotenvy::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
