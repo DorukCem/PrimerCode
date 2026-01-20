136ab073c1f042d492ee057393b1ca5ab915017b
Doruk Cem <doruk.cem@gmail.com>
2026-01-20 15:22:09 +0000

    remove

--- renew-certs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /opt/wilderness
-docker compose stop nginx
-docker run --rm -p 80:80 \
-  -v /opt/wilderness/certbot/conf:/etc/letsencrypt \
-  -v /opt/wilderness/certbot/www:/var/www/certbot \
-  certbot/certbot renew --standalone
-docker compose up -d nginx
